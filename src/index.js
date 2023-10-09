bd11dd5eb7fef16cd53f613ff66c88c096f00f6a
Xiraeth <77112995+Xiraeth@users.noreply.github.com>
2023-10-09 20:12:33 +0300

    Create home page function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import "./style.css";
-import bg from "./bg.jpg";
-import initPage from "./pageLoad";
+import homePage from "./home";
+import menuPage from "./menu";
 
 const content = document.querySelector(".content");
+homePage();
 
-initPage(content);
+const homeBtn = document.querySelector(".home");
+const menuBtn = document.querySelector(".menu");
+const contactBtn = document.querySelector(".contact");
+const headerLinksDiv = document.querySelector(".header-links");
+const headerLinks = document.querySelectorAll(".header-links div");
+
+homeBtn.addEventListener("click", homePage);
+
+headerLinksDiv.addEventListener("click", (e) => {
+  const target = e.target.closest(".header-link");
+  if (!target) return;
+
+  headerLinks.forEach((link) => link.classList.remove("activeTab"));
+  target.classList.toggle("activeTab");
+});
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ module.exports = {
   mode: "development",
   entry: {
     index: "./src/index.js",
-    load: "./src/pageLoad.js",
+    home: "./src/home.js",
+    menu: "./src/menu.js",
+    contact: "./src/contact.js",
   },
   devtool: "inline-source-map",
   devServer: {
